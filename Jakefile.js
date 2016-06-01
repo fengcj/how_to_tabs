87fea3604ca18d4fe89a9261927e6628a8410006
fcj <fcjljy@gmail.com>
2016-06-01 17:37:43 +0800

    Using jake as our build tool

---
+++ Jakefile.js
@@ -0,0 +1,10 @@
+(function(){
+	'use strict';
+	
+
+	desc("Default build");
+	task('default',function(){
+		console.log('\n\n This is default Task');
+		console.log("\n\n Build Is OK...");
+	});
+}());
